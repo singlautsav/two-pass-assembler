2c924f40e7db318539a0f076dc2d1226ec426434
Utsav <utsav18321@iiitd.ac.in>
2019-10-11 22:59:57 +0530

    pass one commands Updated

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ def assembly(text):
         if i=='':
             text.remove(i)
     
+    for line in text:
+        line = text.split(' ')
+        for i in line:
+            if i=='':
+                line.remove(i)
+        if len(line)==2:
+            pass
+            '''Check for second element in the list if its a variable add to symbol table
+                isUsed = true:
+            '''
+        elif len(line)==3:
+            '''Check two if's either it has : or it has DW in line(1) either way program counter will add to symbol
+                isFound = true:    
+            '''
+        elif len(line)==1:
+            '''check Stp command'''
+        
+
 
 
 
