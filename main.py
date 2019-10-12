2ceb4cd8928704dc1927ddf3d28e2a30216f7490
Utsav <utsav18321@iiitd.ac.in>
2019-10-12 17:47:37 +0530

    Corrected a lot

--- main.py
+++ main.py
@@ -6,63 +6,64 @@ except NameError:
 text = file.read()
 text = text.split('\n')
 lines = []
-
 symbol_Table=[]
-
+print(text)
 def passOne(text):
-    flag = 0
-    STP_found = 0
+    
+    STP_found = False
     symbolX = {'name': '', 'isUsed': False, 'isFound': False, 'variableAddress': 0}
     for i in text:
         if i == '':
             text.remove(i)
     programCounter = 0
     for line in text:
-        line = text.split(' ')
+        flag = True
+        line = line.split(' ')
         for i in line:
             if i == '':
                 line.remove(i)
         lines.append(line)
-
+        print(line)
         if len(line) == 2:
             # pass
             '''Check for line[1] in the list if its a variable add to symbol table'''
             for i in symbol_Table:
-                if i['name'] == line[1]:
-                    flag = 1
-            if flag == 0:
-                symbolX['name'] = line[1]
-                symbolX['isUsed'] = True
-                symbol_Table.append(symbolX)
+                print(str(i))
+                if line[1] == i['name']:
+                    # print("Flag changed")
+                    flag = False
+                # print('flag: '+ str(flag))
+            if flag:
+                symbol_Table.append({'name': line[1], 'isUsed': True, 'isFound': False, 'variableAddress': 0})
 
 
         elif len(line) == 3:
             '''Check two if's either it has ':' or it has DW in line(1) either way program counter will add to symbol'''
             if line[0][-1] == ':':
                 # its a label
+                print("hello We are here :P found a label")
                 label = {}
-                label_name = line[1][:-1]
+                label_name = line[0][:len(line[0])-1]
+                print(label_name)
                 for i in symbol_Table:
                     if i['name'] == label_name:
                         label = i
-                        if label['isFound'] == False:
-                            label['isFound'] = True
-                            label['variableAddress'] = programCounter
+                        if i['isFound'] == False:
+                            i['isFound'] = True
+                            i['variableAddress'] = programCounter
                         else:
                             print('error - label already found')
-                    else:
-                        print('error - undefined label')
+                    
 
             # isfound
             elif line[1] == 'DW':
-                label = {}
+                # label = {}
                 label_name = line[1]
                 for i in symbol_Table:
                     if i['name'] == label_name:
-                        label = i
-                        if label['isFound'] == False:
-                            label['isFound'] = True
-                            label['variableAddress'] = line[2]
+                        if i['isFound'] == False:
+                            i['isFound'] = True
+                            i['variableAddress'] = line[2]
                         else:
                             print('error - label already found')
                     else:
@@ -86,18 +87,31 @@ def passOne(text):
 
 
         #now iteration on the symbol table
-        variableAddress_counter = 0
-        for i in symbol_Table:
-            if i['isFound'] == False or i['isUsed'] == False:
-                print('error - they must be true')
-            elif i['variableAddress'] == 0:
-                if variableAddress_counter == 0:
-                    variableAddress_counter += 1
-                    i[3] = programCounter
-                elif variableAddress_counter >= 1:
-                    print('error - more than one symbol with variableAddress missing')
 
         programCounter += 1
 
+
+
 def passTwo():
     pass
+
+
+
+passOne(text)
+
+variableAddress_counter = 0
+for i in symbol_Table:
+    if i['isFound'] == False or i['isUsed'] == False:
+        print('error - they must be true')
+    elif i['variableAddress'] == 0:
+        if variableAddress_counter == 0:
+            variableAddress_counter += 1
+            i[3] = programCounter
+        elif variableAddress_counter >= 1:
+            print('error - more than one symbol with variableAddress missing')
+
+
+print(symbol_Table)
+
+
+
