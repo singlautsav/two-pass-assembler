bbf03e07aa4eac8f91e5ec94224ae81ddf794811
Ritik <ritikgarg.1773@gmail.com>
2019-10-14 12:29:37 +0530

    comments added File IO handled

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 from passes import *
 
 
-fileName = input("Input file address")
+fileName = input("Input file address")        # get the input from filename
 try:
-    file = open(fileName,'r')
+    file = open(fileName, 'r')
 except NameError:
     print("No File Found, Kindly Retry")
-text = file.read()
-text = text.split('\n')
+text = file.read()                         # read from the file
+text = text.split('\n')                    # split them
 # text = text.split('\t')
 
 
-print(text)
+print(text)                               # print them
 
-if passOne(text)==0:
-    ErrorFlag = True
+if passOne(text) == 0:
+    ErrorFlag = True                    # if there is an error in pass one pass then there is an error
     ErrorList.append("Stop Command not found")
 else:
     variableAddress_counter = 0
     for i in symbol_Table:
-        if i['isFound'] == False:
+        if i['isFound'] == False:       # if isFound is false then there is an error
             ErrorFlag = True
             ErrorList.append('error- Symbol Address not Defined: '+ i['name'])
-        elif i['isUsed'] == False:
+        elif i['isUsed'] == False:      # if isUsed is false then there is an error
             ErrorFlag = True
             ErrorList.append('error- Symbol Defined But Not Used: '+ i['name'])
-        elif i['variableAddress'] == -1:
+        elif i['variableAddress'] == -1:  # if variableAddress is positive then there is an error  - more than one symbol missing with variableAddress missing
             if variableAddress_counter == 0:
                 variableAddress_counter += 1
                 # i[3] = programCounterX
             elif variableAddress_counter >= 1:
                 ErrorFlag = True
                 ErrorList.append('error - more than one symbol with variableAddress missing')
-
-print(symbol_Table)
+f_symboltable = open('Symboltable', 'w')
+print(symbol_Table)    # print symbol table and write them in file
+for i in symbol_Table:
+    f_symboltable.write(i['name'] + " " + str(i['variableAddress']) + '\n')
+f_symboltable.close()
 print(lines)
+f_output = open("Output.txt", 'w')
+f_error = open('Errorfile.txt', 'w')
 if ErrorFlag:
-    for err in ErrorList:
+    for err in ErrorList:   # here we are printing all the error and write and close the file
         print(err)
+        f_error.write(err + '\n')
 else:
     passTwo()
-    for i in finalOutput:
-        if i!="":
+    for i in finalOutput:    # else print the final output
+        if i != "":
             print(i)
+            f_output.write(i+'\n')
+f_error.close()
+f_output.close()
--- passes.py
+++ passes.py
@@ -3,17 +3,20 @@ lines = []
 symbol_Table=[]
 ErrorFlag = False
 ErrorList = []
-opCode_Table = {'CLA':0,'LAC':1,'SAC':2,'ADD':3,'SUB':4,'BRZ':5,'BRN':6,'BRP':7,'INP':8,'DSP':9,'MUL':10,'DIV':11,'STP':12, 'DW':13}
+opCode_Table = {'CLA': 0, 'LAC': 1, 'SAC': 2, 'ADD': 3, 'SUB': 4, 'BRZ': 5, 'BRN': 6, 'BRP': 7, 'INP': 8, 'DSP': 9, 'MUL': 10, 'DIV': 11, 'STP': 12, 'DW':13}
+
 
 def lineCheck(line):
-    if line[0][-1]==':':
+    # used to check if line[0] is a label or symbol
+    if line[0][-1] == ':':         # it will have ':' at its end for label
         return 1
     else:
-        return 2
+        return 2                   # else it would be a symbol
+
 
 def passOne(text):
     global programCounterX
-    STP_found = 0
+    STP_found = 0                         # flag to check STP is present in the code
     # symbolX = {'name': '', 'isUsed': False, 'isFound': False, 'variableAddress': -1}
     
     for i in text:
@@ -21,68 +24,72 @@ def passOne(text):
             text.remove(i)
     # programCounter = 0
     for line in text:
-        
         foundFlag = False
         flag = True
-        line = line.split(' ')
+        line = line.split(' ')   # done splitting about " "(space)
         for i in line:
             if i == '':
                 line.remove(i)
         # print(line)
-        lines.append(line)
+        lines.append(line)     # here we are inserting the line list in the main list
         # print(line)
+
+        '''The len of the line can be 1,2,3 and we will now proceed to them'''
         if len(line) == 2:
-            # pass
+            # here all instruction of two words will be handled like 'ADD l1' , 'SUB l2' etc
             # print("2"+ str(line))
-            '''Check for line[1] in the list if its a variable add to symbol table'''
+            '''Check for line[1] in the list if its a variable add to symbol table or label'''
             val = lineCheck(line)
             # print(val)
-            if val ==1:
+            if val == 1:
                 '''check label with STP and CLA'''
                 label_name = line[0][:len(line[0])-1]
                 # print(label_name)
                 for i in symbol_Table:
-                    if i['name'] == label_name:
-                        if i['isFound'] == False:
+                    if i['name'] == label_name:          # check if symbol is already present in the symbol table
+                        if i['isFound'] == False:        # as it is a label its 'isFound' must be false and then make it true else error
                             i['isFound'] = True
-                            i['variableAddress'] = programCounterX
-                            foundFlag = True
+                            i['variableAddress'] = programCounterX  # here we get the address and save it
+                            foundFlag = True           # make the foundFlag true
                         else:
-                            ErrorFlag = True
+                            ErrorFlag = True           # error if isfound is already true because its address can't be redecclareed again
                             ErrorList.append('Label Cannot Be declared Again in Line: ' + str(programCounterX))
                 if foundFlag==False:
+                    # if the foundflag is false then add the new symbol in the table
                     symbol_Table.append({'name': label_name, 'isUsed': False, 'isFound': True, 'variableAddress': programCounterX})
-                if line[1]=='STP':
-                    STP_found=1
-            elif val ==2:
+                if line[1] == 'STP':
+                    STP_found = 1      # shows that STP is present in file else it would be an error
+            elif val == 2:
+                # it is a symbol and check if its already in the symTable else add it
                 for i in symbol_Table:
                     if line[1] == i['name']:
                         flag = False
                 if flag:
-                    symbol_Table.append({'name': line[1], 'isUsed': True, 'isFound': False, 'variableAddress': -1})
+                    symbol_Table.append({'name': line[1], 'isUsed': True, 'isFound': False, 'variableAddress': -1})   # symbol added to the symbolTable
             
 
         elif len(line) == 3:
             '''Check two if's either it has ':' or it has DW in line(1) either way program counter will add to symbol'''
             # print(line)
-            if line[0][-1]==':':
+            if line[0][-1] == ':':
                 # print("hello We are here :P found a label")
-                label_name = line[0][:len(line[0])-1]
+                label_name = line[0][:len(line[0])-1]        # check for label
                 # print(label_name)
                 for i in symbol_Table:
-                    if i['name'] == label_name:
-                        if i['isFound'] == False:
+                    if i['name'] == label_name:           # check if already in symTable
+                        if i['isFound'] == False:         # isFound must be false else error
                             i['isFound'] = True
                             i['variableAddress'] = programCounterX
                             foundFlag = True
                         else:
-                            ErrorFlag = True
+                            ErrorFlag = True             # error because symbol can't be redeclared
                             ErrorList.append('Label Cannot Be declared Again in Line: ' + str(programCounterX))
-                if foundFlag==False:
+                if foundFlag == False:         # if not found, add in the symbol table
                     symbol_Table.append({'name': label_name, 'isUsed': False, 'isFound': True, 'variableAddress': programCounterX})
                 if line[1]=='STP':
                     STP_found=1
             elif line[1] == 'DW':
+                '''DW statement is used to assign the variableAddress to symbol and instruction len will be 3'''
                 label_name = line[0]
                 # print(label_name)
                 for i in symbol_Table:
@@ -95,10 +102,9 @@ def passOne(text):
                             ErrorList.append('Label Cannot Be declared Again in Line: ' + str(programCounterX))
                     # else:
                     #     print('error - undefined label')
-            # is   Found = True
-            # variableAddress = programCounter
+
         elif len(line) == 1:
-            '''check Stp command'''
+            '''check Stp command, if not found will give error'''
             # print(line[0])
             if line[0] == 'CLA':
                 pass
@@ -109,9 +115,6 @@ def passOne(text):
                 ErrorList.append("Invalid Command in Line:" + str(programCounterX))
                 # print("Er)
 
-        '''DOUBT - how to know we are at end of file'''
-        '''also include STP not found error'''
-        #     if the program reaches at end and don't got 'STP' then error
         # print(symbol_Table)
         programCounterX += 1
 
@@ -119,7 +122,8 @@ def passOne(text):
 
 
 def RepresentsInt(s):
-    try: 
+
+    try:
         int(s)
         return True
     except ValueError:
@@ -129,28 +133,29 @@ programCounterP2 = 0
 ErrorFlagPass2 = False
 ErrorListPass2 = []
 
-finalOutput = []
+finalOutput = []            # list to display the final output
+
 
 def checkSTP_CLA(line, lineY):
-        if line[0]=='CLA':
-            lineY += bin(programCounterP2)[2:] +" "
-            lineY += bin(opCode_Table['CLA'])[2:]
+        if line[0] == 'CLA':        # check CLA
+            lineY += bin(programCounterP2)[2:] + " "    # convert programCounter to binary and add to the line
+            lineY += bin(opCode_Table['CLA'])[2:]       # convert opCode_Table 'CLA' into binary as machine code conatins the oppcode
             return False, lineY
-        elif line[0]== 'STP':
-            lineY += bin(programCounterP2)[2:] + " "
-            lineY += bin(opCode_Table['STP'])[2:]
+        elif line[0] == 'STP':
+            lineY += bin(programCounterP2)[2:] + " "  # convert programCounter to binary and add to the line
+            lineY += bin(opCode_Table['STP'])[2:]    # convert opCode_Table 'CLA' into binary as machine code conatins the oppcode
             return False, lineY
         return True, lineY
     
 def checkTwo(line, lineX):
-        if line[0]=='CLA' or line[0]=='STP':
+        if line[0] == 'CLA' or line[0] == 'STP':    # check whether CLA and STP are present else error
             lineX = ''
             ErrorFlagPass2 = True
             ErrorListPass2.append("Inavalid opCode with extra Argument at: " + bin(programCounterP2)[2:])
         else:
-            if line[0][-1]==':':
+            if line[0][-1] == ':':
                 # print("checking this")
-                boolX, lineX = checkSTP_CLA(line[1:],lineX)
+                boolX, lineX = checkSTP_CLA(line[1:], lineX)
                 # print(lineX)
                 if boolX:
                     # lineX = ''
@@ -159,18 +164,18 @@ def checkTwo(line, lineX):
             else:
                 
                 try:
-                    lineX += bin(programCounterP2)[2:]+" "
-                    lineX += bin(opCode_Table[line[0]])[2:] +" "
+                    lineX += bin(programCounterP2)[2:]+" "     # convert pc2 to binary
+                    lineX += bin(opCode_Table[line[0]])[2:] + " "   # convet oppcode to binary
                     if RepresentsInt(line[1]):
-                        lineX +=bin(int(line[1]))[2:]
+                        lineX += bin(int(line[1]))[2:]
                     else:   
                         for symbol in symbol_Table:
-                            if symbol['name']==line[1]:
-                                lineX += bin(symbol['variableAddress'])[2:]
+                            if symbol['name'] == line[1]:         # check for the symbol and if true
+                                lineX += bin(symbol['variableAddress'])[2:]  # add the binary of variableAdd to lineX
                                 foundSymbol = True
-                        if foundSymbol==False:
+                        if foundSymbol == False:
                             lineX = ''
-                            ErrorFlagPass2 = True
+                            ErrorFlagPass2 = True           # else error as symbol couldn't be found
                             ErrorListPass2.append("Could not Find Symbol in the Table:" + line[1])
                 except KeyError:
                     lineX = ''
@@ -185,25 +190,25 @@ def passTwo():
     for line in lines:
         lineX = ''
         foundSymbol = False
-        if len(line)==1:
-            boolX, lineX = checkSTP_CLA(line,lineX)
+        if len(line) == 1:       # for len = 1 checkX - if true - error
+            boolX, lineX = checkSTP_CLA(line, lineX)
             if boolX:
                 ErrorFlagPass2 = True
-                ErrorListPass2.append("Invalid OpCode in at: "+ bin(programCounterP2))[2:]
-        elif len(line)==2:
-            lineX = checkTwo(line,lineX)
+                ErrorListPass2.append("Invalid OpCode in at: " + bin(programCounterP2))[2:]
+        elif len(line) == 2:
+            lineX = checkTwo(line, lineX)
 
-        elif len(line)==3:
-            if line[0][-1]==':':
+        elif len(line) == 3:
+            if line[0][-1] == ':':
                 print("here")
-                lineX = checkTwo(line[1:],lineX)
-            elif line[1]=='DW':
+                lineX = checkTwo(line[1:], lineX)
+            elif line[1] == 'DW':
                 lineX = ''
             else:
                 ErrorFlagPass2 = True
                 ErrorListPass2.append("Invalid Opcode or Extra Arguements at:" + bin(programCounterP2))[2:]
         finalOutput.append(lineX)
-        programCounterP2+=1
+        programCounterP2 += 1
         # lineX = ''
 
 
